2dbe767bfbcc6629e4879b053c973f09dc4c6a78
Lakshay <102456284+Monkfuego@users.noreply.github.com>
2024-09-04 17:49:31 +0530

    Update pii_detective.py

--- PIIDetective/pii_detective.py
+++ PIIDetective/pii_detective.py
@@ -1,7 +1,11 @@
-from file_reader import read_text_from_file
+from keras import models
+from processing import preprocess_image
+from file_reader import output_folder,read_text_from_file,image_extraction
+import os
+import numpy as np
 from pii_regex import pii_regex,doc_pii_regex 
 
-def pii_detect(file):
+def pii_detect_layer1(file):
     results = []
     
     for label, pattern in {**pii_regex, **doc_pii_regex}.items():
@@ -9,7 +13,20 @@ def pii_detect(file):
         if matches:
             for match in matches:
                 results.append((label, match))
-    
     return results
 
+
+
+def pii_detect_layer2(file):
+    preprocess_image(image_extraction(file))
+    model = models.load_model('aadhar_card_model.keras')
+    
+    # Iterate over extracted images and make predictions
+    for filename in os.listdir(output_folder):
+       if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+           img_path = os.path.join(output_folder, filename)
+           img_array = preprocess_image(img_path)
+           prediction = model.predict(img_array)
+           predicted_class = np.argmax(prediction)
+           print(f"Predicted class for {filename}: {predicted_class}")
 
