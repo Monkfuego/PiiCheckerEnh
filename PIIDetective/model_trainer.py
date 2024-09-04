9b8d2ca0ebcdf5308a83c3a780dfa103c615f7fb
Lakshay <102456284+Monkfuego@users.noreply.github.com>
2024-09-04 17:48:38 +0530

    Update model_trainer.py

--- PIIDetective/model_trainer.py
+++ PIIDetective/model_trainer.py
@@ -3,48 +3,28 @@ from tensorflow import keras
 from keras import layers
 from keras._tf_keras.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
-import pandas as pd
-
 # Create a sample dataset
 positive_samples = [
-    "C:/CODES/PiiCheckerModels/adh_pos1.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos2.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos3.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos4.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos5.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos6.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos7.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos8.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos9.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos10.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos11.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos12.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos13.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos14.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos15.jpg",
-    "C:/CODES/PiiCheckerModels/adh_pos16.jpg",
+    "C:/object_detection/imgs/img1.png",
+    "C:/object_detection/imgs/img2.png",
+    "C:/object_detection/imgs/img3.png",
+    "C:/object_detection/imgs/img4.png",
+    "C:/object_detection/imgs/img5.png",
+    "C:/object_detection/imgs/img6.jpg",
+    "C:/object_detection/imgs/img7.jpg",
+    "C:/object_detection/imgs/img8.jpg",
+    "C:/object_detection/imgs/img9.jpg",
 ]
 negative_samples = [
-    "C:/CODES/PiiCheckerModels/adh_neg1.jpg",
-    "C:/CODES/PiiCheckerModels/adh_neg2.jpg",
-    "C:/CODES/PiiCheckerModels/adh_neg3.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos1.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos2.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos3.jpg", 
-    "C:/CODES/PiiCheckerModels/dl_pos4.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos5.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos6.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos7.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos8.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos9.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos10.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos11.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos12.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos13.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos14.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos15.jpg",
-    "C:/CODES/PiiCheckerModels/dl_pos16.jpg",
-
+    'C:/object_detection/negative_imgs/img1.jpg',
+    "C:/object_detection/negative_imgs/img2.jpg",
+    "C:/object_detection/negative_imgs/img3.jpg",
+    "C:/object_detection/negative_imgs/img4.jpg",
+    "C:/object_detection/negative_imgs/img5.jpg",
+    "C:/object_detection/negative_imgs/img6.jpg",
+    "C:/object_detection/negative_imgs/img7.jpg",
+    "C:/object_detection/negative_imgs/img8.jpg",
+    "C:/object_detection/negative_imgs/img9.jpg"
 ]
 
 # Function to load images
@@ -80,10 +60,6 @@ dataset = dataset.shuffle(buffer_size=len(images)).batch(32)
 model = keras.Sequential([
     layers.Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, 3, activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(128, 3, activation='relu'),
-    layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
     layers.Dense(2, activation='softmax')
@@ -95,22 +71,5 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model
-model.fit(dataset, epochs=20)
-
-input_image_path = "C:/CODES/PiiCheckerModels/adh_pos6.jpg"  # Replace with your input image path
-input_image = preprocess_image(input_image_path)
-
-# Make a prediction
-prediction = model.predict(input_image)
-
-# Interpret the prediction
-predicted_class = np.argmax(prediction)  # Get the index of the highest probability
-if predicted_class == 1:
-    print("The input image is predicted to be a positive sample.")
-else:
-    print("The input image is predicted to be a negative sample.")
-
-# Save the model to an HDF5 file
-model.save('aadhar_card_model.h5')
-
-print("Model saved to aadhar_card_model.h5")
+model.fit(dataset, epochs=30)
+model.save('aadhar_card_model.keras')
