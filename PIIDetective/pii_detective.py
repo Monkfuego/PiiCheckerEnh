01089288ebb0bf0b30d33d3cd3ad0f1fead95db4
JosephRemingston <ljremi@gmail.com>
2024-09-05 15:36:08 +0530

    initial commit

--- PIIDetective/pii_detective.py
+++ PIIDetective/pii_detective.py
@@ -1,9 +1,9 @@
 from keras import models
 from processing import preprocess_image
-from file_reader import output_folder,read_text_from_file,image_extraction
+from file_reader import output_folder, read_text_from_file, image_extraction
 import os
 import numpy as np
-from pii_regex import pii_regex,doc_pii_regex 
+from pii_regex import pii_regex, doc_pii_regex 
 
 def pii_detect_layer1(file):
     results = []
@@ -15,20 +15,19 @@ def pii_detect_layer1(file):
                 results.append((label, match))
     return results
 
-
-
 def pii_detect_layer2(file):
-    
     extracted_images = []
     model = models.load_model('aadhar_card_model.keras')
     
     # Iterate over extracted images and make predictions
     for filename in os.listdir(output_folder):
-           extracted_images.append(filename)
-       if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-           img_array = preprocess_image(extracted_images)
-           for images in img_array:
-               prediction = model.predict(images)
-               predicted_class = np.argmax(prediction)
-               print(f"Predicted class for {filename}: {predicted_class}")
-
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            extracted_images.append(filename)
+            img_path = os.path.join(output_folder, filename)
+            img_array = preprocess_image(img_path)  # Preprocess the image
+            
+            # Predict for each preprocessed image
+            for img in img_array:
+                prediction = model.predict(np.expand_dims(img, axis=0))
+                predicted_class = np.argmax(prediction)
+                print(f"Predicted class for {filename}: {predicted_class}")
